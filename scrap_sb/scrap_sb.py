7b2d2ccb7fba3908fa7cc3927e35669f9d81c0a1
akhil08agrawal <akhil08.iitkgp@gmail.com>
2018-05-29 06:37:48 +0530

    update the code scrab_socialblade

--- scrap_sb/scrap_sb.py
+++ scrap_sb/scrap_sb.py
@@ -9,33 +9,25 @@ import csv
 # Accordingly convert them and run them to get different csv files
 # Remember to change the csv file's name at the name so that the list is not over written
 
-
 ###### list_1 ######
 # To download the top 25 channels
 
 # source = requests.get('https://socialblade.com/youtube/').text
 # soup = BeautifulSoup(source,'lxml')
-#
 # row=[]
 # all_links = soup.find_all('div',{'class':'table-cell section-mg'})
-
 # print(all_links)
-
-
 
 ###### list_2 ######
 # To download the top 500 youTube channels most subscribed
 # Remember to check the box most subscribed on the social blade site
 
 source = requests.get('https://socialblade.com/youtube/top/500/mostsubscribed').text
-soup = BeautifulSoup(source,'lxml')
+soup = BeautifulSoup(source, 'lxml')
 row = []
-all_links = soup.find_all('div',{'style':'float: left; width: 350px; line-height: 25px;'})
-
-
+all_links = soup.find_all('div', {'style':'float: left; width: 350px; line-height: 25px;'})
 
 # The common code for all the lists
-
 for link in all_links:
 	row.append(link.find('a'))
 
@@ -49,14 +41,14 @@ channel_id = []
 for i in range(len(row2)):
 
     channel = requests.get('https://socialblade.com'+row2[i]).text
-    soup_channel = BeautifulSoup(channel,'lxml')
+    soup_channel = BeautifulSoup(channel, 'lxml')
 
     channel_a = []
 
-    all_links1 = soup_channel.find_all('div',{'style':'float: right;'})
-	# print(all_links1)
+    all_links1 = soup_channel.find_all('div', {'style': 'float: right;'})
+# print(all_links1)
     for link in all_links1:
-	    channel_a.append(link.find('a',{'class':'core-button -margin core-small-wide ui-black'}))
+        channel_a.append(link.find('a', {'class': 'core-button -margin core-small-wide ui-black'}))
 
     string = str(channel_a)
 
@@ -68,47 +60,14 @@ for i in range(len(row2)):
     print('i=', i)
 
 print(channel_id)
+while '' in channel_id:                        # There are few empty strings in the list
+    channel_id.remove('')                       # This line of code will simply delete it
 
-csvfile = 'channelIdLiist'           # Remember to change the name of the csv file for different lists
+csvfile = 'channel_List'           # Remember to change the name of the csv file for different lists
 
 with open(csvfile, 'w') as output:
     writer = csv.writer(output, lineterminator='\n')
     for val in channel_id:
         writer.writerow([val])
 
-
-	
-# for link in channel1_a:
-# 	print(link['href'])
-		
-
-
-# article = soup.find_all('div',{'class':'table-cell section-rank'})
-# rows =[]
-
-# for i in range(25):
-# 	rows.append(article[i].find('div',{'class':'table-cell section-mg'}))
-# files=[]
-
-# for j in range(25):
-# 	for link in rows[j].find('a'):
-#     files.append(link.get('href'))
-
-# print(files)
-
-
-
-
-# article=[]
-# files =[]
-# division=[]
-	
-
-# soup.find_all('div',{'class':'table-cell section-mg'})
-# division.append()
-# # a = soup.find_all('a')
-
-
-# for link in article[i].find_all('a'):
-#     files.append((link.get('href')))
-# print(files)
+print('The number of channel ids extracted are ', len(channel_id))

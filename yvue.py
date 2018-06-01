c98e73de1032291a3d05ace155e935a1e0915559
akhil08agrawal <akhil08.iitkgp@gmail.com>
2018-06-01 10:47:30 +0530

    updating yvue.py

--- yvue.py
+++ yvue.py
@@ -6,34 +6,22 @@ import shutil
 from requests import get
 import pandas as pd
 import csv
+# from config import api_key
 import os
+
+api_key = 'AIzaSyBQflK2Y8-UCPlAzlbcfA9TUXozs9fC2iU'
+
+
 
 # username = 'marquesbrownlee'
 
-def createFolder(directory):
-    try:
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    except OSError:
-        print('Error: Creating directory. ' +  directory)
+# idofchannel = 'https://www.googleapis.com/youtube/v3/channels?key={'+api_key+'}&forUsername='+username+'&part=id'
 
-# Example createFolder('./data/')
-# Creates a folder in the current directory called data
-
-API = open("api_key.txt", 'r')
-if API.mode == 'r':
-    api_key = API.read()
-
-# Get an api key and put it in a txt file named api_key before implementing this code
-# we are not using the api key directly here as it must not be exposed on a public platform
-# so we are reading it from a .txt file which is ignored while commiting to the changes
-# Id of channel = 'https://www.googleapis.com/youtube/v3/channels?key={'+api_key+'}&forUsername='+username+'&part=id'
 
 # part1-get a list of video ids
 # function for making a list of urls
-
-
-def get_all_video_links_in_channel(channel_id):
+def get_all_video_in_channel(channel_id):
+    
 
     base_video_url = 'https://www.youtube.com/watch?v='
     base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
@@ -42,26 +30,25 @@ def get_all_video_links_in_channel(channel_id):
 
     video_links = []
     url = first_url
-    count = 0
-    while(count < 4):                            # we want to download 4 images from each Channel
-        inp = urllib.request.urlopen(url)
-        resp = json.load(inp)
-
-        for i in resp['items']:
-            if i['id']['kind'] == "youtube#video":
+    count=0
+    # while (count<6):
+    count+=1
+    inp = urllib.request.urlopen(url)
+    resp = json.load(inp)
+    # print("count=",count)
+    for i in resp['items']:
+        # print("i=",i)
+        if i['id']['kind'] == "youtube#video":
+            if len(video_links)<5 :
                 video_links.append(base_video_url + i['id']['videoId'])
-                count += 1
+            else:
+                break    
 
-        try:
-            next_page_token = resp['nextPageToken']
-            url = first_url + '&pageToken={}'.format(next_page_token)
-        # The above 3 lines should be used when you want to download many thumbnails from a channels
-        # the above code navigates to the next page of the same channel
-
-        except:
-            break
-
-
+    # try:
+    #     next_page_token = resp['nextPageToken']
+    #     url = first_url + '&pageToken={}'.format(next_page_token)
+    # except:
+    #     break
     return video_links
 # mkbhdlinks =[]
 
@@ -75,32 +62,39 @@ def download(url, file_name):
             # write to file
             file.write(response.content)
 
+# mkbhdId = 'UCBJycsmduvYEL83R_U4JriQ'
+# myChannelId='UCruH1rzS6lv_wUEajKdVKrg'
+allChannelIds=[]
 
-allChannelIds = []
+# path = "./channelids"          ##making the new directory
+# if not os.path.isdir(path):
+#     os.makedirs(path)
 
 
-with open('/channel_list_top_500.csv') as f:
-    reader = csv.reader(f, delimiter=',')
+with open('/home/rahul/th_data/channel_list_top_500.csv') as f:
+    reader = csv.reader(f,delimiter=',')
     aci = list(reader)
+
+
+
+
+
 
 for j in range(len(aci)):
     allChannelIds.append(aci[j][0])    
 
-print('The number of channels are as under')
-print(len(allChannelIds))
-# allChannelIds = pd.read_csv('./channel_list_top_500.csv',sep=',')
+print(allChannelIds)    
 
-# Making a new folder to save images in
-path = "./Good"          ##making it the new directory to store the good thumbnails
-if not os.path.isdir(path):
-	os.makedirs(path)
+# allChannelIds = pd.read_csv('/home/rahul/channelids.csv',sep=',')
+# print(allChannelIds)
 
-mylinks = []
 for i in range(len(allChannelIds)):
-    mylinks = get_all_video_links_in_channel(allChannelIds[i])
+
+    print("getting links..")
+    mylinks=get_all_video_in_channel(allChannelIds[i]) 
     # print(mylinks)
-    playlist = []
-    print("i = ", i)
+
+    playlist=[]
 
     for r in range(len(mylinks)):
         segments = mylinks[r].rpartition('v=')
@@ -108,7 +102,13 @@ for i in range(len(allChannelIds)):
         # print(segments[2])
     # print(playlist)
 
+
+
+
     # part 2- downloading thumbnails
+
+
+
     name = []
 
     for r in range(len(playlist)):
@@ -119,9 +119,11 @@ for i in range(len(allChannelIds)):
 
     for r in range(len(playlist)):
         url = 'https://img.youtube.com/vi/'+playlist[r]+'/hqdefault.jpg'
-        print(url)
-        filename = './good'+str(i)+'_'+name1[r]+'.jpg'
+        # print(url)
+        filename = '/home/rahul/th_data/'+str(i)+'_'+name1[r]+'.jpg'
         # urllib.request.urlretrieve(url, r)
-        download(url, filename)
-        print("r=", r)
-    print(allChannelIds[i])
+        download(url,filename)
+        print("r=",r)
+    # print(allChannelIds[i])
+
+

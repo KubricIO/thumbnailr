21fff9051e353a091bb009b9d14b9ce4c54c54aa
akhil08agrawal <akhil08.iitkgp@gmail.com>
2018-06-08 17:41:26 +0530

    video_to_frames.py code

---
+++ video_to_frames.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import urllib2
+
+
+video_url = 'https://storage.googleapis.com/videos-kubric/video-c67d4b7f-8ba7-40b6-8269-fe09cb33c18b.mp4'
+file_name = 'trial_video.mp4' 
+rsp = urllib2.urlopen(video_url)
+with open(file_name,'wb') as f:
+    f.write(rsp.read())
+
+# we can also just read in the video without actually downloading it
+
+file1 = file_name
+vidcap = cv2.VideoCapture(file1)
+success,image = vidcap.read()
+
+print (success)
+count = 0
+time = 0
+path = "./frames"          ##making the new directory
+if not os.path.isdir(path):
+    os.makedirs(path)
+success = True
+while success:
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,time)
+    cv2.imwrite(os.path.join(path,"frame1%d.jpg" %count), image)     
+    #save frame as JPEG file
+    success,image = vidcap.read()
+    #print ('Read a new frame: ', success)
+    count += 1
+    time += 500

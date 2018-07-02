629de60de017007d9befa2b72155252eb21ebcfd
akhil08agrawal <akhil08.iitkgp@gmail.com>
2018-07-02 17:38:14 +0530

    updating code 3

--- model_three.py
+++ model_three.py
@@ -143,8 +143,10 @@ def train_top_model():
               validation_data=(validation_data, validation_labels))
     # model.save_weights(top_model_weights_path)
     name = 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    model.save('model_two.h5')
-    os.rename('model_two.h5','model_two_'+name+ 'sgd' +'.h5')
+    model.save('model_two_sgd.h5')
+    # model.save('model_two.h5')
+    # os.rename('model_two.h5','model_two_'+name+ 'sgd' +'.h5')
+
 
     # scores = model.evaluate(test_data, test_labels,
     #                     epochs=epochs,

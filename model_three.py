d2f69f2297ad70c64ee0716a5d977a7997cf462a
akhil08agrawal <akhil08.iitkgp@gmail.com>
2018-07-03 16:38:17 +0530

    updating code with inception idea

--- model_three.py
+++ model_three.py
@@ -136,12 +136,12 @@ def train_top_model():
     Block4.add(Conv2D(256, (1, 1), activation='relu', padding='same', input_shape=train_data.shape[1:]))
     Block4.add(MaxPooling2D(pool_size=(2, 2), strides=1, padding='same'))
 
-    merged = Concatenate( [Block1, Block2, Block3, Block4], axis=channel_axis)
+    merged = Concatenate( [Block1, Block2, Block3, Block4])
 
     model.add(merged)
 
     # Inception over
-    
+
     # model.add(Conv2D(512, (3, 3), activation='relu', padding='same',input_shape=train_data.shape[1:]))
 
     model.add(Flatten())

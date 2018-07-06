4f40426aad7418c5b52eb90eb0cb4c3b19a83eb9
akhil08agrawal <akhil08.iitkgp@gmail.com>
2018-07-06 12:20:49 +0530

    updating feedback loop

--- feedback_loop.py
+++ feedback_loop.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 from PIL import Image
+import subprocess
 
 import numpy as np
 import requests
@@ -8,6 +9,14 @@ import requests
 
 import shutil
 from uuid import uuid4
+
+def create_directory(directory):
+    """
+        :param directory: folder path which needs to be created if not already present
+        :return: nothing
+    """
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 def get_filecount(path_to_directory):
     if os.path.exists(path_to_directory):
@@ -23,35 +32,37 @@ path = '/home/akhil/thumbnailr/overall/'   # change the path to directory
 if not os.path.isdir(path):
     os.makedirs(path)
 
-files = os.listdir(path)
+# files = os.listdir(path)
 count = get_filecount(path)
 print(count)
 
+path1 = './' + 'folder1'
+create_directory(path1)
+path2 = './' + 'folder2'
+create_directory(path2)
+path3 = './' + 'folder3'
+create_directory(path3)
+path4 = './' + 'folder4'
+create_directory(path4)
+path5 = './' + 'folder5'
+create_directory(path5)
+path6 = './' + 'folder6'
+create_directory(path6)
+
 def feedback_loop():
 
-    for i in range(count):
+
+    path = './overall/'
+    create_directory(path)
+    for i in range(5):
         print ("i = ",i)
-        image = Image.open(path + str(i+1) + '.jpg')
+        image = Image.open(path + str(i + 1) + '.jpg',"r")
         image.show()
-        cv2.waitKey(1000)
-        cv2.destroyAllWindows()
+        folder = input()
+        cv2.imwrite(os.path.join(path + '/folder' + str(folder) + './'+ str (i + 1) + ".jpg"), image)
+        os.popen('killall display')
+        image.close()
         i +=1
-
-
-
-    # path1 = './' + 'folder' + str(score)
-    # create_directory(path1)
-    # path2 = './' + 'folder' + str(score)
-    # create_directory(path2)
-    # path3 = './' + 'folder' + str(score)
-    # create_directory(path3)
-    # path4 = './' + 'folder' + str(score)
-    # create_directory(path4)
-    # path5 = './' + 'folder' + str(score)
-    # create_directory(path5)
-    #
-
-
 
 feedback_loop()
 

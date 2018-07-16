7a9c2a319f9948c75bc6c98895f4a282d877f2a7
Maaz Ahmed Sheriff <maazsheriff97@gmail.com>
2018-07-16 16:47:54 +0530

    removed errors

--- model_mark1.py
+++ model_mark1.py
@@ -186,8 +186,8 @@ def train_top_model():
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(4096, kernel_initializer=initializers.glorot_uniform(seed=None), kernel_regularizer=regularizers.l2(0.01),
               activation='relu'))
-    model.add(Dropout(0.6))
-    model.add(Dense(1, activation='softmax'))
+    model.add(Dropout(0.5))
+    model.add(Dense(5, activation='softmax'))
     print('3')
 
     adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)

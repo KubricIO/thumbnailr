b33fc9cd8592069f32e27dd0bf24383962a4a3ce
Maaz Ahmed Sheriff <maazsheriff97@gmail.com>
2018-07-18 18:25:30 +0530

    changes in conv arch no vgg

--- model_mark2.py
+++ model_mark2.py
@@ -146,21 +146,18 @@ validation_generator = test_datagen.flow_from_directory(
 
 model = Sequential()
 
-model.add(Conv2D(128, (3, 3), padding='same', activation = 'relu', input_shape=(150,150,3)))
+model.add(Conv2D(64, (3, 3), padding='same', activation = 'relu', input_shape=(150,150,3)))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3), padding='same', activation = 'relu'))
+model.add(Conv2D(128, (3, 3), padding='same', activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3), padding='same', activation = 'relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-
-model.add(Conv2D(512, (3, 3), padding='same', activation = 'relu'))
+model.add(Conv2D(128, (3, 3), padding='same', activation = 'relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(1024, activation = 'relu'))
-model.add(Dropout(0.4))
+model.add(Dense(256, activation = 'relu'))
+model.add(Dropout(0.3))
 model.add(Dense(5, activation = 'softmax'))
 
 

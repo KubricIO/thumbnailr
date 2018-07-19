7c96540972c9efe3e2a47522dcf281f078f67a67
Maaz Ahmed Sheriff <maazsheriff97@gmail.com>
2018-07-19 11:09:50 +0530

    resnetv2 code

--- model_mark_resnetv2.py
+++ model_mark_resnetv2.py
@@ -46,7 +46,7 @@ def get_filecount(path_to_directory):
 #     return
 
 
-epochs = 10
+epochs = 30
 batch_size = 8
 
 nb_train_1_samples = get_filecount("mark_1/train/rate1")
@@ -109,7 +109,7 @@ def save_bottlebeck_features():
 
     # build the VGG16 network
     model = applications.inception_resnet_v2.InceptionResNetV2(include_top=False, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=3)
-    print('1, VGG16 model has been loaded\n')
+    print('1, resnet_v2 model has been loaded\n')
 
     # For the training data
     generator = datagen.flow_from_directory(
@@ -214,13 +214,13 @@ def train_top_model():
     model.add(Flatten(input_shape=train_data.shape[1:]))
     model.add(Dense(4096, kernel_initializer=initializers.glorot_uniform(seed=None), kernel_regularizer=regularizers.l2(0.01),
         activation='relu'))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.5))
     model.add(Dense(4096, kernel_initializer=initializers.glorot_uniform(seed=None), kernel_regularizer=regularizers.l2(0.01),
                activation='relu'))
-    model.add(Dropout(0.4))
+    model.add(Dropout(0.5))
     model.add(Dense(3, activation='softmax'))
     print('3')
-    checkpointer = ModelCheckpoint(filepath='model_3class.h5', verbose=1, save_best_only=True)
+    checkpointer = ModelCheckpoint(filepath='model_class3_resnetv2.h5', verbose=1, save_best_only=True)
     callbacks_list = [checkpointer]
     adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
     sgd = SGD(lr=1e-4, decay=1e-6, momentum=0.9, nesterov=True)
@@ -264,6 +264,6 @@ def train_top_model():
     # print('4 : Done and Dusted')
 
 
-#save_bottlebeck_features()
+save_bottlebeck_features()
 train_top_model()
 print("\n\ntime taken =", time.clock() - start)

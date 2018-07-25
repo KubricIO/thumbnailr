33e60c8b433f35aec44c31793315fe5f073d89b4
Maaz Ahmed Sheriff <maazsheriff97@gmail.com>
2018-07-25 14:30:10 +0530

    3 cllass with kfold

--- model_mark_resnetv2.py
+++ model_mark_resnetv2.py
@@ -59,14 +59,14 @@ nb_train_3_samples = nb_train_3_samples - nb_train_3_samples % batch_size
 nb_train_samples = nb_train_1_samples + nb_train_2_samples + nb_train_3_samples
 
 
-nb_val_1_samples = get_filecount("mark_1/val/rate1")
-nb_val_2_samples = get_filecount("mark_1/val/rate2")
-nb_val_3_samples = get_filecount("mark_1/val/rate3")
-
-nb_val_1_samples = nb_val_1_samples - nb_val_1_samples % batch_size
-nb_val_2_samples = nb_val_2_samples - nb_val_2_samples % batch_size
-nb_val_3_samples = nb_val_3_samples - nb_val_3_samples % batch_size
-nb_validation_samples = nb_val_1_samples + nb_val_2_samples + nb_val_3_samples
+# nb_val_1_samples = get_filecount("mark_1/val/rate1")
+# nb_val_2_samples = get_filecount("mark_1/val/rate2")
+# nb_val_3_samples = get_filecount("mark_1/val/rate3")
+#
+# nb_val_1_samples = nb_val_1_samples - nb_val_1_samples % batch_size
+# nb_val_2_samples = nb_val_2_samples - nb_val_2_samples % batch_size
+# nb_val_3_samples = nb_val_3_samples - nb_val_3_samples % batch_size
+# nb_validation_samples = nb_val_1_samples + nb_val_2_samples + nb_val_3_samples
 
 
 nb_test_1 = get_filecount("mark_1/test/rate1")
@@ -110,28 +110,28 @@ def save_bottlebeck_features():
 
     print("saving train bottleneck features")
 
-    np.save(open('bottleneck_features_train_3class_resnet.npy', 'wb'),
+    np.save(open('bottleneck_features_train_3class_resnet_kf.npy', 'wb'),
             bottleneck_features_train)
     print("bottleneck features for the training data has been stored")
 
-    # for the validation data
-    generator = datagen.flow_from_directory(
-        validation_data_dir,
-        target_size=(img_width, img_height),
-        batch_size=batch_size,
-        class_mode=None,
-        shuffle=False)
-
-    print("Generating validation bottleneck features")
-
-    bottleneck_features_validation = model.predict_generator(
-        generator, nb_validation_samples // batch_size)
-
-    print("Generating validation bottleneck features")
-
-    np.save(open('bottleneck_features_validation_3class_resnet.npy', 'wb'),
-            bottleneck_features_validation)
-    print("bottleneck features for the validation data has been stored")
+    # # for the validation data
+    # generator = datagen.flow_from_directory(
+    #     validation_data_dir,
+    #     target_size=(img_width, img_height),
+    #     batch_size=batch_size,
+    #     class_mode=None,
+    #     shuffle=False)
+    #
+    # print("Generating validation bottleneck features")
+    #
+    # bottleneck_features_validation = model.predict_generator(
+    #     generator, nb_validation_samples // batch_size)
+    #
+    # print("Generating validation bottleneck features")
+    #
+    # np.save(open('bottleneck_features_validation_3class_resnet.npy', 'wb'),
+    #         bottleneck_features_validation)
+    # print("bottleneck features for the validation data has been stored")
 
     # For the test data
     generator = datagen.flow_from_directory(
@@ -148,53 +148,52 @@ def save_bottlebeck_features():
 
     print("Generating test bottleneck features")
 
-    np.save(open('bottleneck_features_test_3class_resnet.npy', 'wb'),
+    np.save(open('bottleneck_features_test_3class_resnet_kf.npy', 'wb'),
             bottleneck_features_test)
     print("bottleneck features for the test data has been stored")
 
 
 def train_top_model():
     #print('training model...')
-    train_data = np.load(open('bottleneck_features_train_3class_resnet.npy', 'rb'))
+    train_data = np.load(open('bottleneck_features_train_3class_resnet_kf.npy', 'rb'))
     train_labels = np.array([0] * int(nb_train_1_samples) + [1] * int(nb_train_2_samples) + [2] * int(nb_train_3_samples))
 
-    validation_data = np.load(open('bottleneck_features_validation_3class_resnet.npy', 'rb'))
-    validation_labels = np.array([0] * int(nb_val_1_samples) + [1] * int(nb_val_2_samples) + [2] * int(nb_val_3_samples))
+    #validation_data = np.load(open('bottleneck_features_validation_3class_resnet.npy', 'rb'))
+    #validation_labels = np.array([0] * int(nb_val_1_samples) + [1] * int(nb_val_2_samples) + [2] * int(nb_val_3_samples))
 
-    test_data = np.load(open('bottleneck_features_test_3class_resnet.npy', 'rb'))
+    test_data = np.load(open('bottleneck_features_test_3class_resnet_kf.npy', 'rb'))
     test_labels = np.array([0] * int(nb_test_1) + [1] * int(nb_test_2) + [2] * int(nb_test_3))
     train_labels = to_categorical(train_labels, 3)
-    validation_labels = to_categorical(validation_labels, 3)
+    #validation_labels = to_categorical(validation_labels, 3)
     test_labels = to_categorical(test_labels, 3)
 
 
     kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=seed)
-    cvscores = []
     for train, test in kfold.split(train_data, train_labels):
         model = Sequential()
-    # Inception Model
+        # Inception Model
 
-    # Block1 = Sequential()
-    # Block2 = Sequential()
-    # Block3 = Sequential()
-    # Block4 = Sequential()
-    #
-    # Block1.add(Conv2D(256, (1, 1), activation='relu', padding='same',input_shape=train_data.shape[1:]))
-    # Block1.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-    #
-    # Block2.add(Conv2D(256, (1, 1), activation='relu', padding='same',input_shape=train_data.shape[1:]))
-    # Block2.add(Conv2D(64, (5, 5), activation='relu', padding='same'))
-    #
-    # Block3.add(Conv2D(128, (1, 1), activation='relu', padding='same', input_shape=train_data.shape[1:]))
-    #
-    # Block4.add(Conv2D(256, (1, 1), activation='relu', padding='same', input_shape=train_data.shape[1:]))
-    # Block4.add(MaxPooling2D(pool_size=(2, 2), strides=1, padding='same'))
-    #
-    # model.add(Concatenate([Block1, Block2, Block3, Block4],input_shape=train_data.shape[1:]))
+        # Block1 = Sequential()
+        # Block2 = Sequential()
+        # Block3 = Sequential()
+        # Block4 = Sequential()
+        #
+        # Block1.add(Conv2D(256, (1, 1), activation='relu', padding='same',input_shape=train_data.shape[1:]))
+        # Block1.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+        #
+        # Block2.add(Conv2D(256, (1, 1), activation='relu', padding='same',input_shape=train_data.shape[1:]))
+        # Block2.add(Conv2D(64, (5, 5), activation='relu', padding='same'))
+        #
+        # Block3.add(Conv2D(128, (1, 1), activation='relu', padding='same', input_shape=train_data.shape[1:]))
+        #
+        # Block4.add(Conv2D(256, (1, 1), activation='relu', padding='same', input_shape=train_data.shape[1:]))
+        # Block4.add(MaxPooling2D(pool_size=(2, 2), strides=1, padding='same'))
+        #
+        # model.add(Concatenate([Block1, Block2, Block3, Block4],input_shape=train_data.shape[1:]))
 
-    # model.add(merged ,input_shape=train_data.shape[1:])
+        # model.add(merged ,input_shape=train_data.shape[1:])
 
-    # Inception over
+        # Inception over
         model.add(Flatten(input_shape=train_data.shape[1:]))
         model.add(Dense(4096, kernel_initializer=initializers.glorot_uniform(seed=None), kernel_regularizer=regularizers.l2(0.01),
                   activation='relu'))
@@ -210,10 +209,10 @@ def train_top_model():
         #model=load_model('model_class3_resnetv2_dense1_4096_2.h5')
         model.compile(optimizer=optimizer,
                       loss='categorical_crossentropy', metrics=['accuracy'])
-    # print(len(train_data))
-    # print(len(train_labels))
-    # print(len(validation_data))
-    # print(len(validation_labels))
+        # print(len(train_data))
+        # print(len(train_labels))
+        # print(len(validation_data))
+        # print(len(validation_labels))
 
         print("shape of the model output = ", model.output_shape)
         model.fit(train_data[train], train_labels[train],
@@ -221,10 +220,14 @@ def train_top_model():
               batch_size=batch_size,
               validation_data=(train_data[test], train_labels[test]),
               callbacks=callbacks_list)
-    # model.save_weights(top_model_weights_path)
-        #name = 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
-    #model.save('model_test2.h5')
-
+        # model.save_weights(top_model_weights_path)
+            #name = 'Timestamp: {:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())
+        #model.save('model_test2.h5')
+    model.evaluate(test_data, test_labels,
+                            batch_size=batch_size,
+                            verbose=2,
+                            sample_weight=None,
+                            steps=None)
 
     #
     # scores1 = model.predict(test_data, batch_size=batch_size, verbose=2)
@@ -240,6 +243,6 @@ def train_top_model():
     # print('4 : Done and Dusted')
 
 
-#save_bottlebeck_features()
+save_bottlebeck_features()
 train_top_model()
 print("\n\ntime taken =", time.clock() - start)
